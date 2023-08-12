4ac4f8868b1b5dc986d09c9ccab70104ff21579f
Viren Tanti <Virentanti16@gmail.com>
2023-08-12 23:43:23 +0530

    landing page static done

--- src/Pages/Homepage/Components/Landing/Landing.jsx
+++ src/Pages/Homepage/Components/Landing/Landing.jsx
@@ -1,5 +1,35 @@
 import React from "react";
+import "./Landing.css";
+import car1 from "./assets/car-1.png"
+import car2 from "./assets/car-2.png"
 
 export default function Landing() {
-  return <div>Landing</div>;
+  return (
+  <div
+  className="hero">
+    <h1 
+    id="hero-text">
+      ROBOTRIX
+    </h1>
+    <h2
+    id="hero-subtext">
+      Gear up for a high-speed showdown of innovation and skill<br></br>at the Robo Grand Prix - where circuits and bytes collide!
+    </h2>
+    <div
+    className="cars">
+      <img 
+      id="img-1"
+      className="car"
+      src={car1} 
+      alt="car-1">
+      </img>
+      <img 
+      id="img-2"
+      className="car"
+      src={car2}
+      alt="car-2">
+      </img>
+    </div>
+  </div>
+  )
 }
