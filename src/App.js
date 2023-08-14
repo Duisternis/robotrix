8099ebc74040b0a6618b366261013878eb3ba887
Mayank Kumar <mayank.k9802@gmail.com>
2023-08-14 23:02:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
-import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "./Pages/Homepage/Homepage";
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Homepage />} />
+        {/* <Route path="blogs" element={<Blogs />} /> */}
+        {/* <Route path="contact" element={<Contact />} /> */}
+        {/* <Route path="*" element={<NoPage />} /> */}
+      </Routes>
+    </BrowserRouter>
+  );
 }
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
