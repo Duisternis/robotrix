c087c9b3386d9d69458cf429cd6ba5918c534be8
Mayank Kumar <mayank.k9802@gmail.com>
2023-08-15 01:33:00 +0530

    reg

--- src/Pages/Homepage/Components/Register/Register.jsx
+++ src/Pages/Homepage/Components/Register/Register.jsx
@@ -2,20 +2,28 @@ import React from "react";
 // import bgimage from "./Assets/BGimage.png";
 import qr from "./Assets/qr.png";
 import "./Register.css";
+import { Link } from "react-router-dom";
 
 export default function Register() {
   return (
     <div id="register-sec">
       <div className="heading">Register</div>
       <div className="register-section">
-        <form action="/" className="reg-content form" method="post">
-          <input type="text" required placeholder="Name" />
-          <input type="number" required placeholder="Enrollment No." />
-          <input type="email" required placeholder="JUIT Email Id" />
-          <input type="Number" required placeholder="Contact No." />
-        </form>
-        <div className="qrimage">
-          <img src={qr} alt="" />
+        <div className="reg-form">
+          <form action="/" className="reg-content form" method="post">
+            <input type="text" required placeholder="Name" />
+            <input type="number" required placeholder="Enrollment No." />
+            <input type="email" required placeholder="JUIT Email Id" />
+            <input type="Number" required placeholder="Contact No." />
+          </form>
+          <div className="qrimage">
+            <img src={qr} alt="" />
+          </div>
+        </div>
+        <div className="reg-submit-btn">
+          <Link className="reg-submit" to="/">
+            SUBMIT
+          </Link>
         </div>
       </div>
     </div>
