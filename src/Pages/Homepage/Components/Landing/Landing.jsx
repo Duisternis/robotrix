0af412e03939cfc509784c3a0b4d44efb2837a3e
Viren Tanti <Virentanti16@gmail.com>
2023-08-14 13:24:27 +0530

    added links  and assets

--- src/Pages/Homepage/Components/Landing/Landing.jsx
+++ src/Pages/Homepage/Components/Landing/Landing.jsx
@@ -7,14 +7,23 @@ export default function Landing() {
   return (
   <div
   className="hero">
-    <h1 
-    id="hero-text">
-      ROBOTRIX
-    </h1>
-    <h2
-    id="hero-subtext">
-      Gear up for a high-speed showdown of innovation and skill<br></br>at the Robo Grand Prix - where circuits and bytes collide!
-    </h2>
+    <div className="front">
+      <div className="links">
+        <a className="link" href="https://www.instagram.com/ieeejuit/">Instagram</a>
+        <a className="link" href="https://twitter.com/ieee_juit/">Twitter</a>
+        <a className="link" href="https://www.linkedin.com/in/ieeejuitsb/">LinkedIn</a>
+      </div>
+      <div className="title">
+        <h1 
+        id="hero-text">
+          ROBOTRIX
+        </h1>
+        <h2
+        id="hero-subtext">
+          Gear up for a high-speed showdown of innovation and skill<br></br>at the Robo Grand Prix - where circuits and bytes collide!
+        </h2>
+      </div>
+    </div>
     <div
     className="cars">
       <img 
