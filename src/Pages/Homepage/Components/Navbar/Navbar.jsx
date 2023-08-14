8084c1b0e0adccd9a47dc2089f14e589973a0bd3
Mayank Kumar <mayank.k9802@gmail.com>
2023-08-14 23:35:26 +0530

    nav

--- src/Pages/Homepage/Components/Navbar/Navbar.jsx
+++ src/Pages/Homepage/Components/Navbar/Navbar.jsx
@@ -1,29 +1,82 @@
-import React from "react";
-import IEEELogo from "./Assets/ieeelogo.svg";
+import React, { useState, useEffect } from "react";
+import { Link, NavLink } from "react-router-dom";
+// import {HashLink as Link} from 'react-router-hash-link';
 import "./Navbar.css";
+import IEEELogo from "./Assets/ieeelogo.svg";
+import { Cross as Hamburger } from "hamburger-react";
+import { animateScroll as scroll } from "react-scroll";
+import { Link as LNK } from "react-scroll";
+function Navbar() {
+  const [header, setHeader] = useState(false);
 
-export default function Navbar() {
+  const changeBackground = () => {
+    if (window.scrollY >= 10) {
+      setHeader(true);
+    } else {
+      setHeader(false);
+    }
+  };
+
+  window.addEventListener("scroll", changeBackground);
+  const [isOpen, setOpen] = useState(false);
+
+  const [isOpen1, setIsOpen1] = useState(false);
+
+  const onClickHeader = () => {
+    setIsOpen1(!isOpen1);
+  };
   return (
-    <>
-      <nav className="navbar">
-        <a href="">
-          <img src={IEEELogo} alt="" />
-        </a>
-        <div>
-          <a className="nav-items" href="">
-            Home
-          </a>
-          <a className="nav-items" href="">
-            About
-          </a>
-          <a className="nav-items" href="">
-            Schedule
-          </a>
+    <div className={header ? "header active" : "header"}>
+      <div
+        className={`header-wrapper ${isOpen1 ? "header-wrapper active" : ""} `}
+      >
+        <NavLink to="/" className="header-logojyc">
+          <img onClick={"/"} className="header-logo" src={IEEELogo} />
+        </NavLink>
+
+        <div onClick={onClickHeader} className="menu">
+          <Hamburger toggled={isOpen} toggle={setOpen} />
         </div>
-        <a id="register" className="nav-items" href="">
-          Register
-        </a>
-      </nav>
-    </>
+
+        <div className="nav-items">
+          <ul>
+            <li>
+              <Link onClick={() => scroll.scrollToTop()} to="/">
+                HOME
+              </Link>
+            </li>
+
+            <li>
+              <LNK
+                to="about-hp"
+                spy={true}
+                smooth={true}
+                offset={-100}
+                duration={1000}
+              >
+                ABOUT
+              </LNK>
+            </li>
+            <li>
+              <LNK
+                to="about-hp"
+                spy={true}
+                smooth={true}
+                offset={-100}
+                duration={1000}
+              >
+                SCHEDULE
+              </LNK>
+            </li>
+
+            <li>
+              <Link to="/contact">REGISTRATION</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   );
 }
+
+export default Navbar;
